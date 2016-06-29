094549773cb316a213ed5c4d91aeef5cf4d6b4dd
庞晓华 <pxh345946@sina.com>
2016-06-29 09:17:29 +0800

    创建项目
    创建项目

---
+++ myProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myProject
+//
+//  Created by ios on 16/6/28.
+//  Copyright © 2016年 dozer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myProject
+//
+//  Created by ios on 16/6/28.
+//  Copyright © 2016年 dozer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
